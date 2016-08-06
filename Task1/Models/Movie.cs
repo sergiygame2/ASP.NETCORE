d2d3339e3977c596cd3411d8a15b1caab6f9d641
Sergiy S.S <sergiygame2@gmail.com>
2016-08-07 01:45:51 +0300

    Added first task

---
+++ Task1/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieYearViewModel
+    {
+        public List<Movie> movies;
+        public SelectList years;
+        public string movieYear { get; set; }
+    }
+
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
