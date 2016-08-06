bec8862a93d71ff7d4d779a0feda51824b6f5bb2
Sergiy S.S <sergiygame2@gmail.com>
2016-08-07 02:09:01 +0300

    +validation

--- Task1/Models/Movie.cs
+++ Task1/Models/Movie.cs
@@ -15,11 +15,18 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 300)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
